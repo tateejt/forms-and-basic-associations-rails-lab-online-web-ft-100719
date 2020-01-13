63bfa6bb9fc7f1ee4e1988cc243e11ed8350e714
Emery Tate <tateejt@gmail.com>
2020-01-13 04:26:14 +0000

    Automatically backed up by Learn

--- db/migrate/20200113042438_add_genre_to_songs.rb
+++ db/migrate/20200113042438_add_genre_to_songs.rb
@@ -1,4 +1,4 @@
-class AddGenreToSongs < ActiveRecord::Migration[5.0]
+class AddGenreToSongs < ActiveRecord::Migration
   def change
     add_reference :songs, :genre, index: true, foreign_key: true
   end
